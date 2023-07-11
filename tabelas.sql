f458f99f010a195c833cd0a20a8c4e816e6ffb7a
wellisonS <wsp@cin.ufpe.br>
2023-07-11 10:39:00 -0300

    adicionando os relacinamentos

--- tabelas.sql
+++ tabelas.sql
@@ -118,9 +118,85 @@ CREATE TABLE paciente (
     CONSTRAINT paciente_fk FOREIGN KEY (cpf_p) REFERENCES pessoa(cpf)
 );
 
+CREATE TABLE dados_acompanhante (
+    
+    nome VARCHAR2(255),
+    cpf_acompanhante VARCHAR2(5),
+
+    CONSTRAINT dados_acompanhante_pk PRIMARY KEY (cpf_acompanhante)
+
+);
 
 CREATE TABLE acompanhante (
 
+    cpf_acompanhante_fk VARCHAR2(5),
+    cpf_pac VARCHAR2(5),
 
+    CONSTRAINT acompanhante_pk PRIMARY KEY (cpf_acompanhante_fk,cpf_pac),
+    CONSTRAINT acompanhante_dados_fk FOREIGN KEY (cpf_acompanhante_fk) REFERENCES dados_acompanhante (cpf_acompanhante),
+    CONSTRAINT acompanhante_pac_fk FOREIGN KEY (cpf_pac) REFERENCES paciente(cpf_p)
+
+);
+
+
+CREATE TABLE tarja_medicamento (
+
+    tarja VARCHAR2(50) NOT NULL,
+    nome_med VARCHAR2(255),
+
+    CONSTRAINT tarja_medicamento_pk PRIMARY KEY (nome_med)
+);
+
+
+CREATE TABLE medicamento (
+
+    dosagem VARCHAR2(10),
+    nome_medicamento VARCHAR2 (255),
+
+    CONSTRAINT medicamento_pk PRIMARY KEY (dosagem,nome_medicamento),
+    CONSTRAINT medicamento_fk FOREIGN KEY (nome_medicamento) REFERENCES tarja_medicamento (nome)
+
+
+);
+
+-- REALACIONAMENTOS 
+
+CREATE TABLE cirurgia (
+
+    cpf_medico VARCHAR2(5),
+    cpf_enfermeiro VARCHAR2(5),
+    data_cirurgia DATE,
+    cpf_paciente VARCHAR2(5),
+
+    CONSTRAINT cirurgia_pk PRIMARY KEY (cpf_medico,cpf_enfermeiro,data_cirurgia,cpf_paciente),
+    CONSTRAINT cirurgia_med_fk FOREIGN KEY (cpf_medico) REFERENCES medico (cpf_func),
+    CONSTRAINT cirurgia_enf_fk FOREIGN KEY (cpf_enfermeiro) REFERENCES enfermeiro (cpf_func),
+    CONSTRAINT cirurgia_pac_fk FOREIGN KEY (cpf_paciente) REFERENCES paciente (cpf_p)
+);
+
+CREATE TABLE medico_paciente (
+    cpf_medico VARCHAR2(5),
+    cpf_paciente VARCHAR2(5),
+    data_atendimento DATE,
+
+    CONSTRAINT medico_paciente_pk PRIMARY KEY (cpf_medico,cpf_paciente,data_atendimento),
+    CONSTRAINT medico_paciente_med_fk FOREIGN KEY (cpf_medico) REFERENCES medico (cpf_func),
+    CONSTRAINT medico_paciente_pac_fk FOREIGN KEY (cpf_paciente) REFERENCES paciente (cpf_p)
     
 );
+CREATE TABLE preescreve (
+
+    cpf_medico VARCHAR2(5),
+    cpf_paciente VARCHAR2(5),
+    data_atendimento DATE,
+    dosagem VARCHAR2 (10),
+    nome_med VARCHAR2 (255),
+    frequencia VARCHAR2 (50) NOT NULL,
+
+
+    CONSTRAINT preescreve_pk PRIMARY KEY (cpf_medico,cpf_paciente,data_atendimento,dosagem,nome_med),
+    CONSTRAINT preescreve_medpac_fk FOREIGN KEY (cpf_medico,cpf_paciente, data_atendimento) REFERENCES medico_paciente (cpf_medico,cpf_paciente,data_atendimento),
+    CONSTRAINT preescreve_med_fk FOREIGN KEY (nome_med,dosagem) REFERENCES medicamento (nome_medicamento,dosagem)
+);
+
+
