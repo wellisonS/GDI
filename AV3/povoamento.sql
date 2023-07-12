bcf1df550368dd96887a794f80a1c0b93dbfcfbd
Wellison da Silva Pereira <wsp@windows.cin.ufpe.br>
2023-07-12 10:36:41 -0300

    Criando o esqueleto para o povoamento das tabelas

---
+++ AV3/povoamento.sql
@@ -0,0 +1,58 @@
+-- ENTIDADES
+
+-- Endereço
+
+
+
+-- Pessoa 
+
+
+-- Telefone 
+
+
+-- Funcionario 
+
+
+-- Salario 
+
+
+-- Médico 
+
+
+-- Especializacao 
+
+
+-- Enfermeiro 
+
+
+-- Atendente 
+
+
+-- Paciente 
+
+-- Dados Acompanhante 
+
+
+-- Acompanhante 
+
+
+-- Tarja Medicamento
+
+
+-- Medicamento 
+
+
+-- ******************************************************************************
+
+-- RELACIONAMENTOS 
+
+-- Cirurgia 
+
+
+-- Medico-Paciente 
+
+
+-- Preescreve
+
+
+-- Agenda
