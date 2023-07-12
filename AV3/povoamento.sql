a447a0db5fe0abf12af51ad6c3bb6fb310ffb880
Wellison da Silva Pereira <wsp@WINDOWS.CIN.UFPE.BR>
2023-07-12 10:59:23 -0300

    inserindo a primeira pessoa e seu endereco, para comprovar que tá certo.

--- AV3/povoamento.sql
+++ AV3/povoamento.sql
@@ -1,11 +1,59 @@
 -- ENTIDADES
 
 -- Endereço
-
+INSERT INTO endereco (numero,estado,cidade,rua,complemento,cep) VALUES (81,'PE','Camaragibe','Rua Santana','','12345'); 
+INSERT INTO endereco (numero,estado,cidade,rua,complemento,cep) VALUES ()
+INSERT INTO endereco (numero,estado,cidade,rua,complemento,cep) VALUES ()
+INSERT INTO endereco (numero,estado,cidade,rua,complemento,cep) VALUES ()
+INSERT INTO endereco (numero,estado,cidade,rua,complemento,cep) VALUES ()
+INSERT INTO endereco (numero,estado,cidade,rua,complemento,cep) VALUES ()
+INSERT INTO endereco (numero,estado,cidade,rua,complemento,cep) VALUES ()
+INSERT INTO endereco (numero,estado,cidade,rua,complemento,cep) VALUES ()
+INSERT INTO endereco (numero,estado,cidade,rua,complemento,cep) VALUES ()
+INSERT INTO endereco (numero,estado,cidade,rua,complemento,cep) VALUES ()
+INSERT INTO endereco (numero,estado,cidade,rua,complemento,cep) VALUES ()
+INSERT INTO endereco (numero,estado,cidade,rua,complemento,cep) VALUES ()
+INSERT INTO endereco (numero,estado,cidade,rua,complemento,cep) VALUES ()
+INSERT INTO endereco (numero,estado,cidade,rua,complemento,cep) VALUES ()
+INSERT INTO endereco (numero,estado,cidade,rua,complemento,cep) VALUES ()
+INSERT INTO endereco (numero,estado,cidade,rua,complemento,cep) VALUES ()
+INSERT INTO endereco (numero,estado,cidade,rua,complemento,cep) VALUES ()
+INSERT INTO endereco (numero,estado,cidade,rua,complemento,cep) VALUES ()
+INSERT INTO endereco (numero,estado,cidade,rua,complemento,cep) VALUES ()
+INSERT INTO endereco (numero,estado,cidade,rua,complemento,cep) VALUES ()
+INSERT INTO endereco (numero,estado,cidade,rua,complemento,cep) VALUES ()
+INSERT INTO endereco (numero,estado,cidade,rua,complemento,cep) VALUES ()
+INSERT INTO endereco (numero,estado,cidade,rua,complemento,cep) VALUES ()
+INSERT INTO endereco (numero,estado,cidade,rua,complemento,cep) VALUES ()
+INSERT INTO endereco (numero,estado,cidade,rua,complemento,cep) VALUES ()
 
 
 -- Pessoa 
-
+INSERT INTO pessoa (cpf,nome,SEXO,data_nascimento,cep,numero,complemento) VALUES ('12345','Carlos','M',to_date('1997-04-05','yyyy-mm-dd'), '12345','81','');
+INSERT INTO pessoa (cpf,nome,SEXO,data_nascimento,cep,numero,complemento) VALUES ()
+INSERT INTO pessoa (cpf,nome,SEXO,data_nascimento,cep,numero,complemento) VALUES ()
+INSERT INTO pessoa (cpf,nome,SEXO,data_nascimento,cep,numero,complemento) VALUES ()
+INSERT INTO pessoa (cpf,nome,SEXO,data_nascimento,cep,numero,complemento) VALUES ()
+INSERT INTO pessoa (cpf,nome,SEXO,data_nascimento,cep,numero,complemento) VALUES ()
+INSERT INTO pessoa (cpf,nome,SEXO,data_nascimento,cep,numero,complemento) VALUES ()
+INSERT INTO pessoa (cpf,nome,SEXO,data_nascimento,cep,numero,complemento) VALUES ()
+INSERT INTO pessoa (cpf,nome,SEXO,data_nascimento,cep,numero,complemento) VALUES ()
+INSERT INTO pessoa (cpf,nome,SEXO,data_nascimento,cep,numero,complemento) VALUES ()
+INSERT INTO pessoa (cpf,nome,SEXO,data_nascimento,cep,numero,complemento) VALUES ()
+INSERT INTO pessoa (cpf,nome,SEXO,data_nascimento,cep,numero,complemento) VALUES ()
+INSERT INTO pessoa (cpf,nome,SEXO,data_nascimento,cep,numero,complemento) VALUES ()
+INSERT INTO pessoa (cpf,nome,SEXO,data_nascimento,cep,numero,complemento) VALUES ()
+INSERT INTO pessoa (cpf,nome,SEXO,data_nascimento,cep,numero,complemento) VALUES ()
+INSERT INTO pessoa (cpf,nome,SEXO,data_nascimento,cep,numero,complemento) VALUES ()
+INSERT INTO pessoa (cpf,nome,SEXO,data_nascimento,cep,numero,complemento) VALUES ()
+INSERT INTO pessoa (cpf,nome,SEXO,data_nascimento,cep,numero,complemento) VALUES ()
+INSERT INTO pessoa (cpf,nome,SEXO,data_nascimento,cep,numero,complemento) VALUES ()
+INSERT INTO pessoa (cpf,nome,SEXO,data_nascimento,cep,numero,complemento) VALUES ()
+INSERT INTO pessoa (cpf,nome,SEXO,data_nascimento,cep,numero,complemento) VALUES ()
+INSERT INTO pessoa (cpf,nome,SEXO,data_nascimento,cep,numero,complemento) VALUES ()
+INSERT INTO pessoa (cpf,nome,SEXO,data_nascimento,cep,numero,complemento) VALUES ()
+INSERT INTO pessoa (cpf,nome,SEXO,data_nascimento,cep,numero,complemento) VALUES ()
+INSERT INTO pessoa (cpf,nome,SEXO,data_nascimento,cep,numero,complemento) VALUES ()
 
 -- Telefone 
 
