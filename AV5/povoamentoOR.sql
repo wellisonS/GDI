7f2e3eaedb76c034a8477bfb7521b09134de2688
edzs7 <138688257+edzs7@users.noreply.github.com>
2023-08-19 10:38:47 -0300

    Update povoamentoOR.sql

--- AV5/povoamentoOR.sql
+++ AV5/povoamentoOR.sql
@@ -37,3 +37,37 @@ INSERT INTO tabela_medicos VALUES (
         'Dermatologista'
     )
 );
+-- Inserindo valores na tabela de enfermeiros.
+INSERT INTO tabela_enfermeiros VALUES (
+    tp_enfermeiro(
+        'Maria josé Pinto de Souza',
+        'F',
+        TO_DATE('1990-10-10', 'YYYY-MM-DD'),
+        tp_endereco('PE', 'Limoeiro', 'Rua Casablanca', 777, 'Apto 7', '77777'),
+        tp_telefones(tp_telefone(2)),
+        '12445',
+        '50505'
+    )
+);
+INSERT INTO tabela_enfermeiros VALUES (
+    tp_enfermeiro(
+        'João Grilo de Souza',
+        'M',
+        TO_DATE('1960-14-10', 'YYYY-MM-DD'),
+        tp_endereco('PE', 'Camutanga', 'Rua da Cruz', 007, 'Apto 20', '20520'),
+        tp_telefones(tp_telefone(2)),
+        '16785',
+        '50403'
+    )
+);
+INSERT INTO tabela_enfermeiros VALUES (
+    tp_enfermeiro(
+        'Allana Cavalcantti da Silva',
+        'F',
+        TO_DATE('1998-02-10', 'YYYY-MM-DD'),
+        tp_endereco('PE', 'Ferreiros', 'Rua da Palha', 551, 'Apto 12', '20123'),
+        tp_telefones(tp_telefone(2)),
+        '17584',
+        '50111'
+    )
+);
